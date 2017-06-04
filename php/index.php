30289f69e52caf9541bce3b7a5cd3c2077fdfbc1
Benji <santsben@gmail.com>
2017-06-03 20:15:18 -0500

    Add changes in the webscrapping

--- php/index.php
+++ php/index.php
@@ -8,11 +8,30 @@ $html = file_get_html('http://www.bomberosperu.gob.pe/po_diario.asp');
 // Find all links 
 
 $data = [];
+$map = [];
 // Find all article blocks
-$i = 0;
+
 $tr = $html->find('tr.TextoContenido');
+$img = $html->find('img');
 
 foreach ($tr as $item) {
-  $data[] = $item->find('td div', 3)->plaintext;
-}
-print_r($data);
+  $data['id'] = $item->find('td div', 0)->plaintext;
+  $data['number'] = $item->find('td div', 1)->plaintext;
+  $data['created'] = $item->find('td div', 2)->plaintext;
+  $data['address'] = $item->find('td div', 3)->plaintext;
+  $data['emergency_type'] = $item->find('td div', 4)->plaintext;
+  $data['status'] = $item->find('td div', 5)->plaintext;
+  $data['machines'] = $item->find('td div', 6)->plaintext;
+
+  foreach( $img as $element) {
+  	$map['lat'] = $element->src;
+  	$map['long'] = $element->onclick;
+    $data['map'] = $map;
+  }
+
+  //print_r(str_replace("&nbsp;", "",$data));
+
+  $json = json_encode(str_replace("&nbsp;", "",$data));
+
+  print_r($json);
+}  
